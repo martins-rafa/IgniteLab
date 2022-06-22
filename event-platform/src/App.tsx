f82497e235a9a9d9c4054f3e3930617da5a8ea78
Rafa Martins <rafamartins.dev@gmail.com>
2022-06-22 09:03:55 -0300

    Update App with the Event page

--- event-platform/src/App.tsx
+++ event-platform/src/App.tsx
@@ -1,31 +1,9 @@
-import { gql, useQuery } from "@apollo/client";
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`;
-
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Event } from "./pages/Event";
 
 export function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
-
   return (
     <>
-      <h1 className="text-5xl font-bold text-violet-500">Hello Ignite Lab</h1>
-
-      <ul>
-        {data?.lessons.map((lesson) => {
-          return <li key={lesson.id}>{lesson.title}</li>;
-        })}
-      </ul>
+      <Event></Event>
     </>
   );
 }
