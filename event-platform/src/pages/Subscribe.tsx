225e11b76a732b8521862f57a81f077dc862e72c
Rafa Martins <rafamartins.dev@gmail.com>
2022-06-24 10:01:26 -0300

    Add Subscribe page
    Home page to the user sign up for the event

---
+++ event-platform/src/pages/Subscribe.tsx
@@ -0,0 +1,92 @@
+import { gql, useMutation } from "@apollo/client";
+import { useState, FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
+import { Logo } from "../components/Logo";
+
+const CREATE_SUBSCRIBER_MUTATION = gql`
+  mutation CreateSubscriber($name: String!, $email: String!) {
+    createSubscriber(data: { name: $name, email: $email }) {
+      id
+    }
+  }
+`;
+
+export function Subscribe() {
+  const navigate = useNavigate();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const [createSubscriber, { loading }] = useMutation(
+    CREATE_SUBSCRIBER_MUTATION
+  );
+
+  async function handleSubscribe(event: FormEvent) {
+    event.preventDefault();
+
+    await createSubscriber({
+      variables: {
+        name,
+        email,
+      },
+    });
+
+    navigate("/event");
+  }
+
+  return (
+    <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
+      <div className="w-full max-w-[1100px] flex items-center  justify-between mt-20 mx-auto">
+        <div className="max-w-[640px]">
+          <Logo />
+
+          <h1 className="mt-8 text-[2.5rem] leading-tight">
+            Build an <strong className="text-blue-500">Event Platform</strong>
+            <br />
+            from scratch with <strong className="text-blue-500">React</strong>
+          </h1>
+
+          <p className="mt-4 text-gray-200 leading-relaxed">
+            This week you will master the most used technologies in the market
+            to access the best tech job opportunities.
+          </p>
+        </div>
+
+        <div className="min-w-[360px] p-8 bg-gray-700 border border-gray-500 rounded">
+          <strong className="text-2xl mb-6 block">Sign up for Free</strong>
+
+          <form
+            onSubmit={handleSubscribe}
+            className="flex flex-col gap-2 w-full"
+          >
+            <input
+              className="bg-gray-900 rounded px-5 h-14"
+              type="text"
+              placeholder="Full Name"
+              onChange={(event) => setName(event.target.value)}
+            />
+            <input
+              className="bg-gray-900 rounded px-5 h-14"
+              type="email"
+              placeholder="Email"
+              onChange={(event) => setEmail(event.target.value)}
+            />
+
+            <button
+              className="bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
+              type="submit"
+              disabled={loading}
+            >
+              Sign up
+            </button>
+          </form>
+        </div>
+      </div>
+      <img
+        src="/src/assets/code-mockup.png"
+        alt="a vscode screen with typescript code"
+        className="mt-10"
+      />
+    </div>
+  );
+}
